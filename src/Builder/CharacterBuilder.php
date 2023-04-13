bb3d726fa412c62ecfbcf658f42427a38196931d
pheldu95 <phelps.duncan@gmail.com>
2023-04-13 13:40:04 -0500

    CharacterBuilder can accept multiple attack types now. episode 5

--- src/Builder/CharacterBuilder.php
+++ src/Builder/CharacterBuilder.php
@@ -1,55 +1,61 @@
 <?php
-
 namespace App\Builder;
-
 use App\ArmorType\ArmorType;
+use App\ArmorType\IceBlockType;
+use App\ArmorType\LeatherArmorType;
+use App\ArmorType\ShieldType;
 use App\AttackType\AttackType;
+use App\AttackType\BowType;
+use App\AttackType\FireBoltType;
+use App\AttackType\MultiAttackType;
+use App\AttackType\TwoHandedSwordType;
 use App\Character\Character;
-
 class CharacterBuilder
 {
     private int $maxHealth;
     private int $baseDamage;
-    private string $attackType;
+    private array $attackTypes;
     private string $armorType;
-
-    //it returns itself, a common practice in Builder pattern
-    public function setMaxHealth(int $maxHealth): self{
+    public function setMaxHealth(int $maxHealth): self
+    {
         $this->maxHealth = $maxHealth;
-
         return $this;
     }
     public function setBaseDamage(int $baseDamage): self
     {
-        $this->$baseDamage = $baseDamage;
-
+        $this->baseDamage = $baseDamage;
         return $this;
     }
-    public function setAttackType(string $attackType): self
-    {
-        $this->$attackType = $attackType;
 
+    //the "...$attackTypes" means the function can accept any number of arguments. so in this case, any number of attack types
+    public function setAttackType(string ...$attackTypes): self
+    {
+        $this->attackTypes = $attackTypes;
         return $this;
     }
     public function setArmorType(string $armorType): self
     {
-        $this->$armorType = $armorType;
-
+        $this->armorType = $armorType;
         return $this;
     }
-
     public function buildCharacter(): Character
     {
+        $attackTypes = array_map(fn(string $attackType) => $this->createAttackType($attackType), $this->attackTypes);
+        if(count($attackTypes) === 1){
+            $attackType = $attackTypes[0];
+        }else{
+            $attackType = new MultiAttackType($attackTypes);
+        }
         return new Character(
             $this->maxHealth,
             $this->baseDamage,
-            $this->createAttackType(),
+            $attackType,
             $this->createArmorType(),
         );
     }
-    private function createAttackType(): AttackType
+    private function createAttackType(string $attackType): AttackType
     {
-        return match ($this->attackType) {
+        return match ($attackType) {
             'bow' => new BowType(),
             'fire_bolt' => new FireBoltType(),
             'sword' => new TwoHandedSwordType(),
--- src/GameApplication.php
+++ src/GameApplication.php
@@ -9,6 +9,7 @@ use App\AttackType\BowType;
 use App\AttackType\FireBoltType;
 use App\AttackType\MultiAttackType;
 use App\AttackType\TwoHandedSwordType;
+use App\Builder\CharacterBuilder;
 use App\Character\Character;
 
 class GameApplication
@@ -45,10 +46,31 @@ class GameApplication
     public function createCharacter(string $character): Character
     {
         return match (strtolower($character)) {
-            'fighter' => new Character(90, 12, new TwoHandedSwordType(), new ShieldType()),
-            'archer' => new Character(80, 10, new BowType(), new LeatherArmorType()),
-            'mage' => new Character(70, 8, new FireBoltType(), new IceBlockType()),
-            'mage_archer' => new Character(75, 9, new MultiAttackType([new BowType(), new FireBoltType()]), new ShieldType()),
+            'fighter' => $this->createCharacterBuilder()
+                ->setMaxHealth(90)
+                ->setBaseDamage(12)
+                ->setAttackType('sword')
+                ->setArmorType('shield')
+                ->buildCharacter(),
+            'archer' => $this->createCharacterBuilder()
+                ->setMaxHealth(80)
+                ->setBaseDamage(10)
+                ->setAttackType('bow')
+                ->setArmorType('leather_armor')
+                ->buildCharacter(),
+            'mage' => $this->createCharacterBuilder()
+                ->setMaxHealth(70)
+                ->setBaseDamage(8)
+                ->setAttackType('fire_bolt')
+                ->setArmorType('ice_block')
+                ->buildCharacter(),
+            'mage_archer' => $this->createCharacterBuilder()
+                ->setMaxHealth(75)
+                ->setBaseDamage(9)
+                ->setAttackType('fire_bolt', 'bow') // TODO re-add bow!
+                ->setArmorType('shield')
+                ->buildCharacter(),
+
             default => throw new \RuntimeException('Undefined Character'),
         };
     }
@@ -74,5 +96,9 @@ class GameApplication
     private function didPlayerDie(Character $player): bool
     {
         return $player->getCurrentHealth() <= 0;
+    }
+
+    private function createCharacterBuilder(): CharacterBuilder{
+        return new CharacterBuilder();
     }
 }
